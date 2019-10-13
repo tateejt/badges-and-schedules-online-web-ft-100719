ea2ddb3146bfd05e1eab29ce90b2cb3896bfe720
Emery Tate <tateejt@gmail.com>
2019-10-13 20:41:32 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+
